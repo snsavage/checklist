a2d7401a6f99886ce1015adb9a5fb39243de840f
Scott Savage <ssavage@andculture.com>
2021-06-24 09:43:05 -0400

    Force Jenkins build

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -28,7 +28,7 @@ namespace checklist.Controllers
         {
 // Savage: Insert timeout here.
             var rng = new Random();
-            return Enumerable.Range(1, 10).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 100).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
