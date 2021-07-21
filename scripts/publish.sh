fceb04e1885f543df5275ffa9e6e893f9f344c40
Scott Savage <ssavage@andculture.com>
2021-07-21 16:55:33 -0400

    Add publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,1 +1,39 @@
 #!/bin/bash
+
+publish_path="/c/Program Files (x86)/Jenkins/workspace/Checklist-Scott-Savage-Releases-Working/releases"
+
+echo "Publishing Application"
+
+if [[ ! -d "$publish_path" ]] ; then
+    echo "publish path $publish_path could not be found"
+    exit 1
+fi
+
+echo "dotnet clean"
+if ! dotnet clean ; then 
+    echo "dotnet clean could not be completed"
+    exit 1
+fi
+
+echo "dotnet restore"
+if ! dotnet restore ; then
+    echo "dotnet restore could not be completed"
+    exit 1
+fi
+
+echo "dotnet build"
+if ! dotnet build ; then
+    echo "dotnet build could not be completed"
+    exit 1
+fi
+
+echo "dotnet publish"
+if ! dotnet publish --output "$publish_path" ; then
+    echo "dotnet publish could not be completed"
+    exit 1
+fi
+
+echo "Publish complete!"
+
+exit 0
+
